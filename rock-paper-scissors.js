4b478cc6b1c8206a7802ce5b68b1c7e6dfc2cd8e
ASHISH KUMAR <152021705+LEYTHAN-X@users.noreply.github.com>
2024-12-29 13:53:27 +0530

    Adding Readme for This Project

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -109,7 +109,7 @@ function pickComputerMove(){
     let computerMove='';
     if(randomNumber>=0 && randomNumber < 1/3){
         computerMove='rock';
-    }else if(randomNumber >= 1/3 &&         randomNumber < 2/3){
+    }else if(randomNumber >= 1/3 && randomNumber < 2/3){
         computerMove='paper';
     }else if(randomNumber>=2/3 && randomNumber < 1){
         computerMove='scissors';       
